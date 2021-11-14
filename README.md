d0d18c25adbb889cadca556a6400b77b30dd6548
Southern Comfort <scottguenther94@gmail.com>
2021-11-13 19:31:06 -0500

    Major updates

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# Note-Taker
+
+# Note-Taker            
+## Description
+a web application that can be used to write and save notes
+
+## Table of Contents
+* [Usage](#usage)
+* [Screenshots](#screenshots) 
+* [License](#license)
+* [Contributing](#contributing)
+* [Questions](#questions)
+* [Link](#link)            
+            
+## Usage
+Enter the title and text for your note then click the save icon to save it. You can access a saved note by clicking its title on the left side of the screen. If you want to delete a note, click the trash bin icon next to it.
+
+## Screenshots
+![Note Taker intro page](./images/index.png)
+
+Click on the "+" icon to add a new note then enter the title and text.
+
+![Note Taker intro page](./images/notes1.png)
+
+Click on the save icon to save the note.
+
+![Note Taker intro page](./images/notes2.png)
+
+To access a saved note click on its title. To delete a note, click the trash bin icon beside it.
+
+![Note Taker intro page](./images/notes3.png)
+
+            
+
+
+## Link
+
+            
+## Questions
---
+++ routes/apiRoutes.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+
+module.exports = function (app) {
+  // API GET Requests
+  app.get('/api/notes', function (req, res) {
+    fs.readFile('./db/db.json', (err, data) => {
+      if (err) throw err;
+      dbData = JSON.parse(data);
+      res.send(dbData);
+    });
+  });
+
+  // API POST Requests 
+  app.post('/api/notes', function (req, res) {
+
+    fs.readFile('./db/db.json', (err, data) => {
+      if (err) throw err;
+      dbData = JSON.parse(data);
+      dbData.push(req.body);
+      let number = 1;
+      dbData.forEach((note, index) => {
+        note.id = number;
+        number++;
+        return dbData;
+      });
+      console.log(dbData);
+
+      stringData = JSON.stringify(dbData);
+
+      fs.writeFile('./db/db.json', stringData, (err, data) => {
+        if (err) throw err;
+      });
+    });
+    res.send('Your note has been saved');
+  });
+
+  // API DELETE Requests
+  app.delete('/api/notes/:id', function (req, res) {
+    const deleteNote = req.params.id;
+    console.log(deleteNote);
+
+    fs.readFile('./db/db.json', (err, data) => {
+      if (err) throw err;
+
+      dbData = JSON.parse(data);
+      for (let i = 0; i < dbData.length; i++) {
+        if (dbData[i].id === Number(deleteNote)) {
+          dbData.splice([i], 1);
+        }
+      }
+      console.log(dbData);
+      stringData = JSON.stringify(dbData);
+
+      fs.writeFile('./db/db.json', stringData, (err, data) => {
+        if (err) throw err;
+      });
+    });
+    res.status(204).send();
+  });
+};
